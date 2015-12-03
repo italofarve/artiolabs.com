ccc97f286b2aab42f893f2854c662822dce6b66f
hugosalas91 <hugosalas91@gmail.com>
2015-12-03 16:41:19 +0100

    Hugo 03/12/2015: Eliminada columna derecha de acceso a la intranet y añadida política de cookies.

---
+++ js/politicaDeCookies.js
@@ -0,0 +1,7 @@
+function controlcookies() {
+         // si variable no existe se crea (al clicar en Aceptar)
+    localStorage.controlcookie = (localStorage.controlcookie || 0);
+ 
+    localStorage.controlcookie++; // incrementamos cuenta de la cookie
+    barraaceptacion.style.display='none'; // Esconde la política de cookies
+}
